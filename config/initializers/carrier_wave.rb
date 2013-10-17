0e85f1f171d9f2d22666c623c6d00caa2e071f03
Oleg Keene <ol.keene@gmail.com>
2013-10-17 22:16:19 +0300

    protocol change

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,6 +6,6 @@ CarrierWave.configure do |config|
   config.ftp_user = 'prooflet@windfallstudio.com'
   config.ftp_passwd = 'Abcd3fg*'
   config.ftp_folder = '/prooflet_uploads'
-  config.ftp_url = "ftp://windfallstudio.com#{config.ftp_folder}_#{Rails.env.to_s}"
+  config.ftp_url = "http://windfallstudio.com#{config.ftp_folder}_#{Rails.env.to_s}"
   config.ftp_passive = false # false by default
 end
